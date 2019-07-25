7e6c1f86ce1e8c5e12d38719ffcfe3ecdefd2b04
Andrew Williams <1williamsaj@gmail.com>
2019-07-25 04:10:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,17 @@
 def reverse_each_word(sentence1)
-  new_word = sentence1.split("")
-  new_sentence = []
-
-  new_word.each do |word|
-    end_loop = word.length
-    new_string = ""
-    while end_loop > 0
-     end_loop -= 1
-     new_string += word[end_loop]
-   end
-   new_sentence << new_string
- end
- new_sentence.join('')
+  words = sentence1.split(" ") #splitting string into an Array of words
+  	new_sentence = []
+  	words.each do |word|
+  		# doing the same thing as reverse_whole_string
+  		# but with eah word from an array
+  		end_loop = word.length
+  		new_string = ''
+  		while end_loop > 0
+  			end_loop -= 1
+  			new_string += word[end_loop] #Reversing every letter of each word
+  		end
+  		new_sentence << new_string # appending every word to an new Array
+  		# or you could just do this: new_sentence << reverse_whole_string(word)
+  	end
+  	new_sentence.join(' ') #joining Array into a string delimited by spaces
 end
