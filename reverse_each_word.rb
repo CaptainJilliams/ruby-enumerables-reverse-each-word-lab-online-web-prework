283624858c6c487c11c1b0e882c6f7c63decbb5d
Andrew Williams <1williamsaj@gmail.com>
2019-07-25 03:16:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence1)
   reversed_string = []
 
   sentence1.each do |char|
-    reversed_string.reverse(char)
+    reversed_string.unshift!(char)
   end
   return reversed_string.join('')
 end
