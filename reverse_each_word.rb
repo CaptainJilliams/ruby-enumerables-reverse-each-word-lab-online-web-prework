8518c0eba3fce442a77c9afd4d6068f907fc6ebb
Andrew Williams <1williamsaj@gmail.com>
2019-07-25 04:06:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,6 @@
 def reverse_each_word(sentence1)
-  our_string = sentence1.split("")
-  reversed_string = []
-
-  our_string. each do |letter|
-    reversed_string.unshift(letter)
-  end
-
-  new_word = reversed_string.join("")
-  new_sentence = []
+  new_word = sentence1.split("")
+  
 
   new_word.each do |word|
     end_loop = new_word.length
