042651c9504f40312f4057fdd9cbab2e1fcb298f
Andrew Williams <1williamsaj@gmail.com>
2019-07-25 03:24:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   new_array = []
   new_array << sentence1
 
-  sentence1.each do |memo, (key, value)|
+  new_array.each do |memo, (key, value)|
     memo[key] = value
     memo
   end
